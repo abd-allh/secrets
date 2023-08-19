4e4a729f5ea7314827afa955d779b3dc27decc80
abd-allh <abdallah.ismail698@gmail.com>
2023-08-19 22:52:46 +0300

    Create passwordCheck Middleware

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 //The password is ILoveProgramming
 import express from "express"
-const app = express()
-const port = 3000
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+const app = express()
+const port = 3000
+
+var userIsAuthorised = false
+
 app.use(express.urlencoded({ extended: true }))
-// app.use(express.static("public"))
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"]
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true
+  }
+  next()
+}
+app.use(passwordCheck)
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 })
 
 app.post("/check", (req, res) => {
-  if (req.body.password === "ILoveProgramming") {
+  if (userIsAuthorised) {
     res.sendFile(__dirname + "/public/secret.html")
   } else {
     res.redirect("/")
